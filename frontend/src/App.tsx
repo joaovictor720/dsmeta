c5174ba26ea0b726d81c79f0a14e1e0fd1e88aec
João Victor <silvajoao720@gmail.com>
2022-11-19 18:22:27 -0300

    Toastify added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import Header from "./assets/components/Header"
 import NotificationButton from "./assets/components/NotificationButton"
 import SalesCard from "./assets/components/SalesCard"
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
     <>
+      <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
